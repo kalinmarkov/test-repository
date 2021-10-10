c550814218baf6c56014559b986ce4c649d695df
kalinmarkov <kalin_markoff@mail.bg>
2021-10-11 00:34:13 +0300

    Added POST request to create issue

--- tests/api_tests/github_issues_tests.py
+++ tests/api_tests/github_issues_tests.py
@@ -39,15 +39,37 @@ class GithubIssuesTest(unittest.TestCase):
         if self.skip_tests:
             raise Exception("Skipped")
 
-        url = f"https://api.github.com/search/issues?q=author:{owner} type:{type}"
+        try:
+            url = f"https://api.github.com/search/issues?q=author:{owner} type:{type}"
 
-        issues_response = requests.get(url)
-        json_issues_response = issues_response.json()
-        issues = json_issues_response['items']
+            issues_response = requests.get(url)
+            json_issues_response = issues_response.json()
+            issues = json_issues_response['items']
 
-        print("Print issues titles where author is the owner:")
-        for issue in issues:
-            print(issue['title'])
+            assert len(issues) > 0
+            print("Print issues titles where author is the owner:")
+            for issue in issues:
+                print(issue['title'])
+
+        except:
+            raise Exception("#### ERROR: Searched issues not found!")
+
+    def test_03_post_github_issue(self):
+        print(f'========= {self._testMethodName} =========')
+
+        url = f"https://api.github.com/repos/{owner}/{repo}/issues"
+        headers = {
+            "accept": "application/vnd.github.v3+json"
+        }
+        title = {
+            "title": "issue TEST title"
+        }
+
+        self.__class__.response = requests.post(url, headers, title)
+        status_code = self.response.status_code
+
+        assert self.response.status_code == 201, f"#### ERROR: Response not OK -> {self.response.status_code}"
+        print(f">> POST request for {url} return response 201 Created")
 
     @classmethod
     def tearDown(cls) -> None:
